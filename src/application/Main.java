a15aaaf5598a2ce748a07352fd4ee3e8329d6c07
Walid9595 <Walid.ali125@yahoo.com>
2016-03-24 00:08:20 +0200

    first commit empty project

---
+++ src/application/Main.java
@@ -0,0 +1,31 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.HBox;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	String path = "";
+	Media media;
+	MediaPlayer mediaPlayer ;
+	@Override
+	public void start(Stage primaryStage) {
+		HBox hbox = new HBox();
+		
+		primaryStage.getIcons().add(new Image("file:logo1.png"));
+		
+		Scene scene = new Scene(hbox);
+		primaryStage.setTitle("MediaPlayer");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
